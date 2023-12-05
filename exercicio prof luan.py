692f85e1273c6dfdeb5c5ce3c1d67de669a87fe2
pedro-python <152664826+pedro-python@users.noreply.github.com>
2023-12-04 22:03:30 -0300

    Add files via upload

---
+++ exercicio prof luan.py
@@ -0,0 +1,37 @@
+class Carro:
+
+    def _init_(self, cor , marca , modelo):
+
+        self.cor = cor
+        self.marca = marca
+        self.modelo = modelo 
+        self.ligado = False
+        self.andando = False
+
+        print(self.status())
+        
+    def motor_ligado(self):
+        if self.ligado = True
+            print(motor, 'Ligado')
+        else:
+            def motor_desligado(self):
+              if self.desligado = False
+                print(motor, 'Desligado')
+
+
+
+
+
+        
+        
+
+    
+
+
+              
+
+
+
+
+
+
